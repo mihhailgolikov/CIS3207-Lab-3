35f13a394613f038ebbef47beb6157df3a00a53b
Misha Golikov - tug42787 <misha1998@gmail.com>
2020-04-06 06:51:39 -0400

    Update server.h
    Included wordSearch function declaration and some #include files to help along with the rest of the project.

--- server.h
+++ server.h
@@ -1,17 +1,16 @@
-#define DEFAULT_DICTIONARY "dictionary.txt"
-#define DEFAULT_PORT 8888
+#ifndef SERVER_H
+#define SERVER_H
 
-#include <pthread.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdio.h> 
+#include <pthread.h> // mutexes, threads, and condition variables
+#include <string.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h> 
+#include <unistd.h>
 
-int processWrd(char*);
-int isWrdANum(char*);
+//Search words from the dictionary
+int wordSearch(char list_of_words[][MAX_WORD_SIZE], char* wordToFind);
 
 #endif
