7525c8cb7f714e31875fcfc4b8099750ec63324a
Misha Golikov - tug42787 <misha1998@gmail.com>
2020-04-07 05:36:16 -0400

    Update server.c
    Added a logging function for the server, where we take the given phrase out of the log buffer with a log mutex lock and wait to consume from the log buffer, while the log bugger is empty. Then we remove the given words out of the log buffer and write it to the file, repeating for every needed phrase (and reseting the values as such).

--- server.c
+++ server.c
@@ -40,6 +40,54 @@ int main(int argc, char** argv) {
 
 }
 
+void* logThreadFunc(void* arg) {
+	while(1) {
+
+	  // take the given phrase out of the log buffer (by a log mutex lock), 
+	  // and while log buffer is empty, wait to consume from log buffer
+		pthread_mutex_lock(&log_mutex); 
+		while(amountofLogs == 0) { 
+			pthread_cond_wait(&log_cv_cs, &log_mutex); 
+		}
+
+	
+	  // remove the words from the log buff and write it to the log file, and if the log buff is empty we print out a message
+		char* wordsToLog;
+		if (amountofLogs == 0) {
+			printf("log buffer is empty! Can't remove anything!\n"); 
+		} else { 
+   
+	  // else, we take the word from the log buffer, and store it be be removed from the log buffer
+	  // then reset the log's front queue when it reaches the 100th item.
+			wordsToLog = BUFF_LOG_ARRAY[LOG_QUEUE_FRONT]; 
+			LOG_QUEUE_FRONT = (LOG_QUEUE_FRONT + 1) % LOG_BUF_LEN; 
+			amountofLogs--; // -1 the count of phrases in the log buff
+		}
+		
+		printf("Phrase taken out of log buffer to append to log file: %s", wordsToLog); // FOR TESTING
+		
+    // open and access the log file!
+		logFile_ptr = fopen("logFile.txt", "a"); 
+		if (logFile_ptr == NULL) { // if the file doesnt exist...
+			printf("Cannot open log file!\n"); // error
+			exit(1);
+		}
+
+	// we put the words taken out of the log buffer to the log file,
+	// where fputs returns positive int if the operation is successful
+	// and if not, it returns EOF
+		if (fputs(wordsToLog, logFile_ptr) >= 0) { 
+			printf("Successfully added to log file!\n"); // success!
+		} else { 
+      // otherwise there was an error adding it ot the log file
+			printf("Cannot put word(s) from log buffer to log file.\n"); // print error message
+		}
+		fclose(logFile_ptr); // close the file pointer
+		pthread_mutex_unlock(&log_mutex); // unlock the log mutex
+		pthread_cond_signal(&log_cv_pd); // we signal the log buffer as not full anymore (reseting)
+	}
+}
+
 int wordSearch(char dictionary[][MAX_WORD_SIZE], char* wordToFind) {
 	int i = 0; // i is the index to keep track of words within the dictionary as we go through it,
   
