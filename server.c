882a8d625cc0b761cb1de6eff10503b270ab7276
Misha Golikov - tug42787 <misha1998@gmail.com>
2020-04-07 20:22:50 -0400

    Add files via upload
    Finished implementing functionality for worker thread, as well as the log thread. Connected wordSearch to the worker thread, as well as connected log thread to worker thread. Finished minor debugging, error handling. Added more error messages, and updated function declarations within server.h

--- server.c
+++ server.c
@@ -1,14 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h> // for working with threads, mutexes, and condition variables
+#include <string.h> // used for comparing args at start ie. checking for port number vs. dictionary file
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h> // need for sockaddr_in struct to use with accept()
+#include <unistd.h>
 #include "server.h"
+
 pthread_t threadPool[NUM_WORKER_THREADS], logThread; // thread pool declaration
 pthread_mutex_t job_mutex, log_mutex; // mutex declaration for job and log
 pthread_cond_t job_cv_cs, job_cv_pd; // job buffer condition var (consume/produce)
 pthread_cond_t log_cv_cs, log_cv_pd; // log buffer condition var (consume/produce)
+int BUFF_JOB_ARRAY[JOB_BUF_LEN]; // int array for client connection socket
+char BUFF_LOG_ARRAY[LOG_BUF_LEN][PHRASE_SIZE]; // words to be written to log
+int lengthJob = JOB_BUF_LEN; // job buffer size
+int lengthLog = LOG_BUF_LEN; // log buffer size
+int amountOfJobs = 0; // number of items in job buff
+int amountofLogs = 0; // number of items in log buff
+int JOB_QUEUE_FRONT = -1; // front node of job buff
+int JOB_QUEUE_BACK = 0; // rear node
+int LOG_QUEUE_FRONT = -1; // front node of log buff
+int LOG_QUEUE_BACK = 0; // rear node
+
+int portConnectionNumber = 0; // global declaration portConnectionNumber
+char* dictionaryTextFileName = ""; // global declaration dictionaryTextFileName to be used
+char dictionaryWordsArray[DICTIONARY_SIZE][MAX_WORD_SIZE]; // operational dictionary to be obtained from dictionary file
+int currentWordInDictionary = 0; // keeps track of word count, used when searching for word
+
+struct sockaddr_in client; // struct for client incoming connection socket
+int lengthClient = sizeof(client); // size of client's connection
+int socketConnection; // existance?
+int socketClient; // existance/state
+
+char* CLIENT_MSG = "You're successfully connected to the server, please enter word(s) to check!\n";
+char* MSG_REQ = "Please send me more word(s) to check, or press ESC -> ENTER to escape the server.\n";
+char* MSG_ESC = "Server is now closed, goodbye!\n";
 char ERR_JOB_MUTEX_INIT[] = "***ERROR: Cannot start job mutex.***\n";
 char ERR_LOG_MUTEX_INIT[] = "***ERROR: Cannot start log mutex. ***\n";
 char ERR_CV_JOB_CONSUME[] = "***ERROR: Cannot initialize condition variable for consume job buffer.***\n";
 char ERR_CV_JOB_PRODUCE[] = "***ERROR: Cannot initialize condition variable for produce job buffer.***\n";
 char ERR_CV_LOG_CONSUME[] = "***ERROR: Cannot initialize condition variable for consume log buffer.***\n";
 char ERR_CV_LOG_PRODUCE[] = "***ERROR: Cannot initialize condition variable for produce log buffer.***\n";
+char ERR_NO_PORT[] = "***ERROR: Port cannot be used, please try the default port.***\n";
+char ERR_INVALID_ARGS[] = "***ERROR: Cannot run server with the given arguments, try again.***\n";
+char ERR_INVALID_SOCKET[] = "***ERROR: Invalid socket, could not establish connection.***\n";
+char ERR_JOB_BUFF_FULL[] = "***ERROR: The job buffer is full right now, please wait a second.***\n";
+char ERR_LOG_BUFF_FULL[] = "***ERROR: The log buffer is full right now, please wait a second.***\n";
+
+char WORK_THREAD_INIT_SUCCESS[] = "Worker thread successfully created!\n";
+char LOG_THREAD_INIT_SUCCESS[] = "Log thread successfully created!\n";
+char DICTIONARY_INIT_SUCCESS[] = "Assigned your custom dictionary file successfully!\n";
+char USER_ESC[] = "You have escaped the server! If you'd like to continue, please reconnect!\n";
+
+FILE* logFile_ptr; // log file pointer to open log file with
 
 int main(int argc, char** argv) {
 	if(pthread_mutex_init(&job_mutex, NULL) != 0) { 
@@ -141,6 +186,113 @@ int main(int argc, char** argv) {
 			return -1;
 		}
 
+		//  inside job mutex, we add the socket client to the job buff
+		pthread_mutex_lock(&job_mutex); // we lock the mutex for it too
+		while(amountOfJobs == JOB_BUF_LEN) { // while the job buffer is full
+			pthread_cond_wait(&job_cv_pd, &job_mutex); // we wait for it to free up to have space
+		}
+
+		// socket client goes into job buffer
+		if (amountOfJobs == JOB_BUF_LEN) { // if job buffer is full
+			write(1,ERR_JOB_BUFF_FULL,strlen(ERR_JOB_BUFF_FULL)); 
+
+		} else {
+	  // if the job buffer has no jobs in it, take the first item in the queue and put it at the front (initialize the rear as well)
+			if (amountOfJobs == 0) {
+				JOB_QUEUE_FRONT = 0;
+				JOB_QUEUE_BACK = 0;
+			} 
+			BUFF_JOB_ARRAY[JOB_QUEUE_BACK] = socketClient; // add our socket client to job buff
+			amountOfJobs++; // +1 to socket descriptor count, because we have a successful connection
+			JOB_QUEUE_BACK = (JOB_QUEUE_BACK + 1) % JOB_BUF_LEN; // the job queue resets after every 100th job, in order
+			// to have a circular process of operations, otherwise the buffer would fill up too fast
+		} 
+
+		pthread_mutex_unlock(&job_mutex); // we unlock the job mutex
+		pthread_cond_signal(&job_cv_cs); // create a signal that its not empty anymore
+		printf("Connection established successfully!\n"); // 
+		send(socketClient, CLIENT_MSG, strlen(CLIENT_MSG), 0);  // send out a message to client to prompt them to have a word to 
+		// spell check
+	}
+	return 0;
+}
+
+
+void* workerThreadFunc(void* arg) {
+	while (1) { // inf loop
+		// take the socket descriptor out of the job buffer to be able to use it
+		pthread_mutex_lock(&job_mutex); // lock the job mutex 
+    	
+		while(amountOfJobs == 0) { // while the buffer is empty
+			pthread_cond_wait(&job_cv_cs, &job_mutex); // the worked thread waits for a signal that it IS NOT empty
+		}
+    	// initalize the socket descriptor to be used with the job buffer
+		int socketDesc; 
+		// remove it from the job buff
+		if (amountOfJobs == 0) { // if the job buffer is empty
+			write(1,ERR_JOB_BUFF_FULL,strlen(ERR_JOB_BUFF_FULL));
+		} else { // else, remove it from the job buff
+			socketDesc = BUFF_JOB_ARRAY[JOB_QUEUE_FRONT]; // store the socket descriptor to process it
+			JOB_QUEUE_FRONT = (JOB_QUEUE_FRONT + 1) % JOB_BUF_LEN; // repeated process after 100 tasks
+			amountOfJobs--; // socket descriptor removed, -1 to the job count
+		}
+		pthread_mutex_unlock(&job_mutex); // job mutex is now unlocked 
+		pthread_cond_signal(&job_cv_pd); // job buffer is not full anymore because we took something out, signal as so
+		// we allocate memory for a single word at a time that we're recieving from the client, by calling calloc
+		char* word = calloc(MAX_WORD_SIZE, 1); 
+		// we use recv() with our socket descriptor that we took out of our job buffer, which acts as taking the word from the client and storing it for processing
+		while(recv(socketDesc, word, MAX_WORD_SIZE, 0)) { 
+			if (strlen(word) <= 1) { // if nothing entered, continue and repeat
+				continue;
+			}
+			// exit functionality
+			if (word[0] == 27) { // if the user escapes the client (enter ESC), we close the socket descriptors
+				printf("Socket descriptor: %d..\n", socketDesc); 
+				write(1, USER_ESC, strlen(USER_ESC)); // user escaped server
+				write(socketDesc, MSG_ESC, strlen(MSG_ESC)); // let the server know we closed it 
+				close(socketDesc); // we close the client socket
+				break;
+			}
+			//// SEARCH FUNCTIONALITY !!! ////////////////////////////////////////////////////
+			if (wordSearch(dictionary, word)) { // call our word search (word found)
+				strtok(word, "\n"); // every word will automatically have a newline char at the end, so remove it
+				word = realloc(word, sizeof(char*)*PHRASE_SIZE);
+				 // reallocate the memory to be able to fit the "OK" status at the end of the word and store it within the log
+				strcat(word, " OK\n"); // add OK to the end of the word (correct)
+			} else { // word not found!
+				strtok(word, "\n"); // take out \n
+				word = realloc(word, sizeof(char*)*PHRASE_SIZE); // reallocate space to put "WRONG" status at the end and store it within log
+				strcat(word, " WRONG\n"); // add WRONG to the end
+			}
+			write(socketDesc, word, strlen(word)); // write the new phrase with the status to the clinet
+			write(socketDesc, MSG_REQ, strlen(MSG_REQ)); // print out MSG_REQ
+			// the phrase is now written to the log buff using mutual exclusion
+			pthread_mutex_lock(&log_mutex); // we get the log mutex locked
+			while(amountofLogs == LOG_BUF_LEN) {// while the log buff is full
+				pthread_cond_wait(&log_cv_pd, &log_mutex); // we wait for it to open up
+			}
+			// add the word plus its status to the log buff
+			if (amountofLogs == LOG_BUF_LEN) { // if the log buff is full
+				write(1, ERR_LOG_BUFF_FULL, strlen(ERR_LOG_BUFF_FULL)); // say its full
+			} else {
+		// if the buffer is empty, we load an item into the queue and update the position to front, as well as the rear
+				if (amountofLogs == 0) {
+					LOG_QUEUE_FRONT = 0;
+					LOG_QUEUE_BACK = 0;
+				}
+				strcpy(BUFF_LOG_ARRAY[LOG_QUEUE_BACK], word); // we add the words and their spell status to the log file
+				amountofLogs++; // +1 so we go on to the next word or set of words
+				 // we reset the log queue so that the process continues itself and becomes circular (like a path beginning and ending at same place)
+				LOG_QUEUE_BACK = (LOG_QUEUE_BACK + 1) % LOG_BUF_LEN;
+			}
+
+			pthread_mutex_unlock(&log_mutex); // the log mutex is unlocked
+			pthread_cond_signal(&log_cv_cs); // the log buffer is not empty anymore, so we signal it
+			free(word); // free the space used by the previous word or words to be used for the next word(s)
+			word = calloc(MAX_WORD_SIZE, 1); // and assign the memory for the next word(s)
+		}
+		close(socketDesc); // descriptor is closed when the client is finished
+	}
 }
 
 void* logThreadFunc(void* arg) {
@@ -194,7 +346,7 @@ void* logThreadFunc(void* arg) {
 int wordSearch(char dictionary[][MAX_WORD_SIZE], char* wordToFind) {
 	int i = 0; // i is the index to keep track of words within the dictionary as we go through it,
   
-	// while i <  wordsInDictionary - 1 (because start i = 0)
+	// while i <  currentWordInDictionary - 1 (because start i = 0)
 	while(i < dictionaryWordCount - 1) { // iteration through the dictionary
 		if (strcmp(dictionary[i], wordToFind) == 0) { // if word found
 			return 1; // return word found
--- server.h
+++ server.h
@@ -2,15 +2,39 @@
 #define SERVER_H
 
 #include <stdlib.h>
-#include <stdio.h> 
-#include <pthread.h> // mutexes, threads, and condition variables
-#include <string.h> 
+#include <stdio.h>
+#include <pthread.h> // for working with threads, mutexes, and condition variables
+#include <string.h> // used for comparing args at start ie. checking for port number vs. dictionary file
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h> 
+#include <netinet/in.h> // need for sockaddr_in struct to use with accept()
 #include <unistd.h>
 
-//Search words from the dictionary
-int wordSearch(char list_of_words[][MAX_WORD_SIZE], char* wordToFind);
+// Defined constants
+#define DEFAULT_PORT 7000 // to be used if no port specified by  user
+#define DEFAULT_DICTIONARY "dictionary.txt" // to be used if user does not specify a dictionary to use
+#define NUM_WORKER_THREADS 3 // number of worker threads to be created at start
+#define DICTIONARY_SIZE 200425 // size of dictionary file to be read in
+#define JOB_BUF_LEN 100 // size of job buffer which holds socket descriptors
+#define LOG_BUF_LEN 100 // size of log buffer
+#define MAX_WORD_SIZE 15 // max word size possible in dictionary
+#define PHRASE_SIZE 25 // max phrase size - to be able to concatenate and include 'OK' or 'WRONG' on end of word
+
+
+/********* Function Declartion ***********/
+
+/* This section was taken from the slides provided and nearly word from 
+    word from the book. Essentially, it creates the listener file descriptor, 
+    opens the socket descriptor, sets the socketopt. */
+int open_listenfd(int);
+
+/* both the worker thread function that prompts the user for their input,
+  and checks the word against the dictionary. The log thread which is used
+  to log all the activity from the clients requests to the server */
+void *workerThreadFunc(void *);
+void *logThreadFunc(void *);
+
+//search words from Dictionary.txt
+int searchForWordInDict(char list_of_words[][MAX_WORD_SIZE], char* wordToFind);
 
 #endif
